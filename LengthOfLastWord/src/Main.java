bc4fc230df80e0e03c60cc6b6b5908f29e0fa051
AshrafGamalG3 <159867026+AshrafGamalG3@users.noreply.github.com>
2024-03-14 23:40:56 +0200

    files

--- LengthOfLastWord/src/Main.java
+++ LengthOfLastWord/src/Main.java
@@ -2,12 +2,12 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(lengthOfLastWord("Hello world"));
     }
-    public int lengthOfLastWord(String s) {
+    public static int lengthOfLastWord(String s) {
         String v = s.trim();
         int l = 0;
-        for (int i = v.length(); i >= 0; i--) {
+        for (int i = v.length()-1; i >= 0; i--) {
             l++;
             if (v.charAt(i) == ' ') {
                 break;
